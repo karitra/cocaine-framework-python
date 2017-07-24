f6ac6ab6721724da62111cee62f44fe4898548db
karitra <karev.alexandre@gmail.com>
2017-07-24 20:34:50 +0300

    docs: written

--- cocaine/detail/secadaptor.py
+++ cocaine/detail/secadaptor.py
@@ -1,13 +1,16 @@
-#
-# Mostly compy-pasted from cocaine-tools
-#
-from tornado import gen
-from tornado.ioloop import IOLoop
+'''Secure service adaptor.
 
-from cocaine.services import Service
-from cocaine.exceptions import CocaineError
+Provides method for Service adaptor construction, capable to wrap Cocaine
+service and inject security token into the header of service request.
 
+Note: mostly copy-pasted from cocaine-tools `SecureService` implementation.
+'''
 import time
+
+from tornado import gen
+
+from .service import Service
+from ..exceptions import CocaineError
 
 
 class SecureServiceError(CocaineError):
@@ -15,16 +18,39 @@ class SecureServiceError(CocaineError):
 
 
 class Promiscuous(object):
+    '''Null token fetch interface implementation.
+
+    Used for fallback in case of unsupported (not set) secure module type
+    provided by user, access errors due empty token will be propagated
+    to caller code.
+    '''
     @gen.coroutine
     def fetch_token(self):
         raise gen.Return('')
 
 
 class TVM(object):
+    '''Tokens fetch interface implementation.
 
+    Provides public `fetch_token` method, which should be common
+    among various token backends types.
+
+    Attributes:
+        TYPE (str): String representation of token fetcher type,
+            will be included in secure header.
+    '''
+    # Can be taken from class name in case of TVM, but could be inconvenient
+    # in less general name formatting rules.
     TYPE = 'TVM'
 
     def __init__(self, client_id, client_secret, name='tvm'):
+        '''TVM
+
+        Args:
+            client_id (int): Integer client identificator.
+            client_secret (str): Client secret.
+            name (str): TVM service name, defaults to 'tvm'.
+        '''
         self._client_id = client_id
         self._client_secret = client_secret
 
@@ -32,20 +58,31 @@ class TVM(object):
 
     @gen.coroutine
     def fetch_token(self):
+        '''Gains token from secure backend service.
+        Returns:
+            token (str): formatted for cocaine protocol header token.
+        '''
         grant_type = 'client_credentials'
 
-        channel = yield self._tvm.ticket_full(self._client_id, self._client_secret, grant_type, {})
+        channel = yield self._tvm.ticket_full(
+            self._client_id, self._client_secret, grant_type, {})
         ticket = yield channel.rx.get()
 
-        raise gen.Return(self._make_header(ticket))
+        raise gen.Return(self._make_token(ticket))
 
-    def _make_header(self, ticket):
+    def _make_token(self, ticket):
         return '{} {}'.format(self.TYPE, ticket)
 
 
 class SecureServiceAdaptor(object):
-
+    '''Wrapper for injecting service method with secure token.
+    '''
     def __init__(self, wrapped, secure, tok_update_sec=None):
+        '''
+        Args:
+            wrapped (cocaine.Service): Cocaine service.
+            secure: Tokens provider with `fetch_token` implementation.
+        '''
         self._wrapped = wrapped
         self._secure = secure
 
@@ -67,17 +104,18 @@ class SecureServiceAdaptor(object):
     @gen.coroutine
     def _get_token(self):
         try:
+            # TODO: Seems too many branches with common ending.
             if self._to_expire:
                 if time.time() > self._to_expire:
                     # tok_update_sec should be set in __init__ when
                     # self._to_expire is valid
                     self._token = yield self._secure.fetch_token()
-                    self._to_expire = time.time() + self.tok_update_sec
+                    self._to_expire = time.time() + self._tok_update_sec
                 elif not self._token:  # init state
                     self._token = yield self._secure.fetch_token()
             else:
                     self._token = yield self._secure.fetch_token()
-        except Exception as e:
+        except Exception as err:
             raise SecureServiceError(
                 'failed to fetch secure token: {}'.format(err))
 
@@ -97,7 +135,16 @@ class SecureServiceAdaptor(object):
 class SecureServiceFabric(object):
 
     @staticmethod
-    def make_secure_adaptor(service, mod, client_id, client_secret, tok_update_sec=None):
+    def make_secure_adaptor(
+            service, mod, client_id, client_secret, tok_update_sec=None):
+        '''
+        Args:
+            service (cocaine.Service): Service to wrap in.
+            mod (str): Name (type) of token refresh backend.
+            client_id (int): Client identificator.
+            client_secret (str): Client secret.
+            tok_update_sec (int, optional): Token update interval in seconds.
+        '''
         if mod == 'TVM':
             return SecureServiceAdaptor(
                 service, TVM(client_id, client_secret), tok_update_sec)
--- cocaine/services/__init__.py
+++ cocaine/services/__init__.py
@@ -23,8 +23,8 @@
 
 from ..detail.channel import EmptyResponse
 from ..detail.locator import LOCATOR_DEFAULT_ENDPOINTS, Locator
-from ..detail.service import Service
 from ..detail.secadaptor import SecureServiceFabric
+from ..detail.service import Service
 
 __all__ = [
     "Service", "Locator", "EmptyResponse", "LOCATOR_DEFAULT_ENDPOINTS",
